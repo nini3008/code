3a8655e046bb13eb49cb29a3926221d92a12eaca
Nicholas Iloba <nick_iloba@hotmail.com>
2018-12-03 07:41:01 -0500

    created first component storepicker

---
+++ react/catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class StorePicker extends React.Component {
+  // Determines what goes on the page
+  render() {
+    return <p>i am the StorePicker</p>;
+  }
+}
+
+export default StorePicker;
