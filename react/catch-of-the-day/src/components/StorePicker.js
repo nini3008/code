2d51d5047aae08c729eaf8027e5d1d4847fd85a7
Nicholas Iloba <nick_iloba@hotmail.com>
2018-12-04 00:22:07 -0500

    made progress with react tutorial

--- react/catch-of-the-day/src/components/StorePicker.js
+++ react/catch-of-the-day/src/components/StorePicker.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   // Determines what goes on the page
   render() {
-    return <p>i am the StorePicker</p>;
+    return (
+      <form className="store-selector">
+        <h2> Please Enter A Store </h2>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
   }
 }
 
