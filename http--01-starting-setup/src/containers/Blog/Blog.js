d20c1a8a8415128c9664ac7cd91ea7886fcd1c0a
Nicholas Iloba <nick_iloba@hotmail.com>
2019-03-14 22:57:21 -0400

    30% into http with react

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: "Nick"
+        };
+      });
+      this.setState({
+        posts: updatedPosts
+      });
+    });
+  }
+  render() {
+    const Posts = this.state.posts.map(post => {
+      return <Post key={post.id} title={post.title} author={post.author} />;
+    });
+    return (
+      <div>
+        <section className="Posts">{Posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
